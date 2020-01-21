d18685bc2a00ff8a729272aa9487d8dacf8b2146
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-01-21 11:01:54 +0800

    Moved ng files to root

--- server.js
+++
@@ -1,8 +0,0 @@
-var spawn = require('child_process').exec;
-
-spawn('twitter-proxy');
-spawn('http-server');
-
-console.log('Server running on http://localhost:8080');
-console.log('Request the Twitter API using: http://localhost:7890/1.1/statuses/user_timeline.json\?count\=30\&screen_name\=makeschool');
-
